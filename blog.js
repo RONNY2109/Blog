bb6ff0f4e4d58558553400284a8adcf0e823eb05
RONNY2109 <ronnycaraballo2109@gmail.com>
2025-06-17 13:11:47 -0400

    first commit

---
+++ blog.js
@@ -0,0 +1,29 @@
+const result = () =>
+{
+	let score=0;
+	if(document.getElementById('correct1').checked)
+		{	score++;
+		}
+	if(document.getElementById('correct2').checked)
+		{	score++;
+		}
+	if(document.getElementById('correct3').checked)
+		{	score++;
+		}
+ alert(""+score);
+ 
+  
+  
+
+};
+
+window.addEventListener("scroll", function () {
+  let texto = document.getElementById("texto");
+  let scrollY = window.scrollY;
+
+  if (scrollY > 100) { // Ajusta el valor según necesites
+    texto.style.opacity = "0"; // Desaparece
+  } else {
+    texto.style.opacity = "1"; // Aparece
+  }
+});
